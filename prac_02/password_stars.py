8cac3e81dffa697d7b622e9de3567b80d4d509a0
Khushi <khushisachin.parikh@my.jcu.edu.au>
2026-09-23 13:51:00 +0800

    Allow user to input their password

---
+++ prac_02/password_stars.py
@@ -0,0 +1,20 @@
+PASSWORD = "******"
+
+def main():
+    """Password checker"""
+    print("Welcome to the password checker!")
+
+    password = input("Enter your password: ")
+
+#     while password != PASSWORD:
+#         print("Incorrect password")
+#         password = input("Enter your password: ")
+#
+#     print("Access granted!")
+#
+# def is_valid(password):
+#     """Check if password is valid"""
+#     return password == PASSWORD
+#
+# if __name__ == "__main__":
+#     main()
